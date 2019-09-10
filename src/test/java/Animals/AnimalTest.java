aa2c4e81134f884b284180203c21aaeae07d8065
Ruben <ruben>
2019-09-10 14:13:11 +0200

    Eerste Unit tests

--- src/test/java/Animals/AnimalTest.java
+++ src/test/java/Animals/AnimalTest.java
@@ -2,16 +2,45 @@ package Animals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
 
+    private Animal animal;
+
     @BeforeEach
     void setUp() {
+        animal = new Animal("Ugly Duckling", Gender.Male);
     }
 
     @AfterEach
     void tearDown() {
     }
+
+    @Test
+    void TestConstructorNameAreEqual(){
+        //Arrange
+        //Act
+        //Assert
+        assertEquals("Ugly Duckling", this.animal.Name);
+    }
+
+    @Test
+    void TestConstructorGenderAreEqual(){
+        //Arrange
+        //Act
+        //Assert
+        assertEquals(Gender.Male, this.animal.Gender);
+    }
+
+    @Test
+    void TestConstructorReservedIsNull(){
+        //Arrange
+        //Act
+        //Assert
+        assertNull(this.animal.reservedBy);
+    }
+
 }
