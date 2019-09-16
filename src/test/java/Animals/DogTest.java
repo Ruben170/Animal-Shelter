93eeb2249afc57eac687ba81470fcf695e96f516
Ruben <ruben>
2019-09-16 17:36:29 +0200

    checkout

--- src/test/java/Animals/DogTest.java
+++ src/test/java/Animals/DogTest.java
@@ -1,14 +1,33 @@
 package Animals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DogTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    private Dog dog;
+
+    @BeforeEach
     void setUp() {
+        dog = new Dog("Lilo", Gender.Female);
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
+    }
+
+    //Constructor tests
+
+    @Test
+    void TestNameEqual(){
+        assertEquals("Lilo", dog.Name);
+    }
+
+    @Test
+    void TestGenderEqual(){
+        assertEquals(Gender.Female, dog.Gender);
     }
 }
